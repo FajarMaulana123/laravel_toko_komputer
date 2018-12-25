bf3d6c318039fa65116b08fedb4803952f18d265
youngarifswag6690 <arifbro6690@gmail.com>
2018-12-25 23:56:40 +0700

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+@section('content')
+	
+	<div class="jumbotron text-center">
+		<h1>{{$title}}</h1>
+   		<p>Fajar Maulana</p>
+   		<p><a class="btn btn-primary btn-lg" href="/login" role="button">login</a> <a class="btn btn-primary btn-lg" href="/register" role="button">register</a></p>
+	</div>
+
+    
+
+    
+@endsection
